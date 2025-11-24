3551e8056f8593f05b5193503e0f8e1edff17996
nay-fi <nurayukf@gmail.com>
2025-11-24 22:05:19 +0700

    Commit #7:Update Footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,32 +1,43 @@
 <!-- Footer -->
 <div class="bg-gray-900 block">
-    <footer class="w-full inline-flex justify-between items-center align-middle gap-4 p-8">
-        <p class="text-gray-500 text-lg">&copy; 2023 Company, Inc</p>
+    <footer class="w-full inline-flex justify-between gap-4 px-24 py-16">
+        <div>
+            <p class="text-gray-50 font-bold">Email</p>
+            <p class="text-gray-400 text-sm my-4">nurayukfebreyantui@gmail.com</p>
+            <br>
+            <p class="text-gray-50 font-bold">Address</p>
+            <p class="text-gray-400 text-sm my-4">Starly Digital</p>
+        </div>
+        <div>
+            <ul>
+                <li><a href="" class="text-gray-50 font-bold text-md">Quick Links</a></li>
+                <li class="my-4"><a href="/" class="text-gray-400 text-sm">Home</a></li>
+                <li class="my-4"><a href="{{ route('index.about') }}" class="text-gray-400 text-sm">About</a></li>
+                <li class="my-4"><a href="{{ route('index.project') }}" class="text-gray-400 text-sm">Project</a></li>
+                <li class="my-4"><a href="{{ route('index.about')}}" class="text-gray-400 text-sm">Contact</a></li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li><a href="#" class="text-gray-50 font-bold text-</a>md">Explore</li>
+                <li class="my-4"><a href="#" class="text-gray-400 text-sm">Project Demos</a></li>
+                <li class="my-4"><a href="#" class="text-gray-400 text-sm">Case Studies</a></li>
+                <li class="my-4"><a href="#" class="text-gray-400 text-sm">Testimonials</a></li>
+                <li class="my-4"><a href="#" class="text-gray-400 text-sm">Event & Webinars</a></li>
+                <li class="my-4"><a href="#" class="text-gray-400 text-sm">Photography</a></li>
+            </ul>
+        </div>
 
-        <a href="/" class="flex items-center align-middle">
-            <svg class="center" width="40" height="32">
-                <img src="/assets/images/stardig.png" alt="" class="w-24">
-            </svg>
-        </a>
+        <div>
+            <img src="/assets/images/stardig.png" alt="" class="w-24">
+            <p class="text-wrap line-clamp-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse, quia!</p>
 
-        <ul class=" inline-flex inline-blockjustify-end">
-            <li class="nav-item"><a href="" class="px-4 text-gray-700">Home</li></a>
-            <li class="nav-item"><a href="" class="px-4 text-gray-700">About</li></a>
-            <li class="nav-item"><a href="" class="px-4 text-gray-700">Project</li></a>
-            <li class="nav-item"><a href="" class="px-4 text-gray-700">Testimonials</li></a>
-            <li class="nav-item"><a href="" class="px-4 text-gray-700">Contact</li></a>
-        </ul>
+        </div>
+
 
 
     </footer>
     <div class="w-full inline-flex justify-between items-center align-middle gap-4 border-t-2 px-24 py-4">
-        <p class="text-gray-500 text-sm">&copy; 2023 Company, Inc</p>
-
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-            <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
-            <path
-                d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
-        </svg>
-
+        <p class="text-gray-500 text-sm">&copy; 2025 Created by Nur Ayuk</p>
     </div>
 </div>
